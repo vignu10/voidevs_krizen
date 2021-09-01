ceeeadb3dcaaa9780b1522b538ee10125c5373ea
vignu10 <vignuday2501@gmail.com>
2021-09-01 14:43:07 +0530

    commit

--- form.js
+++ form.js
@@ -1,3 +1,19 @@
+ // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyBUd1tT_CE3TibpZCQ2dsmf9bBPNZ8Ga60",
+    authDomain: "krizen-4cc77.firebaseapp.com",
+    projectId: "krizen-4cc77",
+    storageBucket: "krizen-4cc77.appspot.com",
+    messagingSenderId: "710745925047",
+    appId: "1:710745925047:web:d906d1ac65eea46cfbb432",
+    measurementId: "G-X17CZKGBYC"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+
 //signupfunction
 function signUp()
 {
@@ -5,7 +21,7 @@ function signUp()
     var password=document.getElementById("password");
      
 
-    const promise=auth.createUserWithEmailAndPassword(email.value.password.value);
+    const promise=auth.createUserWithEmailAndPassword(email.value,password.value);
     promise.catch(e=>alert(e.message));
     alert("SignUp Successful");
 }
@@ -15,7 +31,7 @@ function logIn(){
     var email=document.getElementById("email");
     var password=document.getElementById("password");
 
-    const promise=auth.signInWithEmailAndPassword(email.value.password.value);
+    const promise=auth.signInWithEmailAndPassword(email.value,password.value);
     promise.catch(e=>alert(e.message));
 
 }
