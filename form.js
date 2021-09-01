dd8d025621444e6f5824ae51723f51da251d0de8
vignu10 <vignuday2501@gmail.com>
2021-09-01 17:36:56 +0530

    krizen

--- form.js
+++ form.js
@@ -1,6 +1,4 @@
- // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
+var firebaseConfig = {
     apiKey: "AIzaSyBUd1tT_CE3TibpZCQ2dsmf9bBPNZ8Ga60",
     authDomain: "krizen-4cc77.firebaseapp.com",
     projectId: "krizen-4cc77",
@@ -9,52 +7,46 @@
     appId: "1:710745925047:web:d906d1ac65eea46cfbb432",
     measurementId: "G-X17CZKGBYC"
   };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-
-//signupfunction
-function signUp()
-{
-    var email=document.getElementById("email");
-    var password=document.getElementById("password");
-     
-
-    const promise=auth.createUserWithEmailAndPassword(email.value,password.value);
-    promise.catch(e=>alert(e.message));
-    alert("SignUp Successful");
-}
-
-//login
-function signIn(){
-    var email=document.getElementById("email");
-    var password=document.getElementById("password");
-
-    const promise=auth.signInWithEmailAndPassword(email.value,password.value);
-    promise.catch(e=>alert(e.message));
-
-}
-
-//logout
-
-
-function signOut()
-{
-    auth.signOut();
-    alert("Successfully logged out")
-}
-
-
-//active User to homePage
-
-firebase.auth().onAuthStateChanged((user)=>{
-    if(user)
-    {
-        var email=user.email;
-        alert("Active user"+email);
+    const auth =  firebase.auth();
+  
+    //signup function
+    function signUp(){
+      var email = document.getElementById("email");
+      var password = document.getElementById("password");
+  
+      const promise = auth.createUserWithEmailAndPassword(email.value,password.value);
+      //
+      promise.catch(e=>alert(e.message));
+      alert("SignUp Successfully");
     }
-    else{
-        alert("No Active User found ")
+  
+    //signIN function
+    function  signIn(){
+      var email = document.getElementById("email");
+      var password  = document.getElementById("password");
+      const promise = auth.signInWithEmailAndPassword(email.value,password.value);
+      promise.catch(e=>alert(e.message));
+      
     }
-})
+  
+  
+    //signOut
+  
+    function signOut(){
+      auth.signOut();
+      alert("SignOut Successfully from System");
+    }
+  
+    //active user to homepage
+    firebase.auth().onAuthStateChanged((user)=>{
+      if(user){
+        var email = user.email;
+        alert("Active user "+email);
+  
+      }else{
+        alert("No Active user Found")
+      }
+    })
