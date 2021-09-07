2d2b1b6e883c6b7a1bd2389248eb8610cfab60bf
vignu10 <vignuday2501@gmail.com>
2021-09-07 12:45:39 +0530

    styling changed

--- form.js
+++ form.js
@@ -12,17 +12,35 @@ var firebaseConfig = {
     firebase.initializeApp(firebaseConfig);
 
     const auth =  firebase.auth();
-  
+   
     //signup function
-    function signUp(){
+     function signUp(){
       var email = document.getElementById("email");
       var password = document.getElementById("password");
   
-      const promise = auth.createUserWithEmailAndPassword(email.value,password.value);
-      promise.catch(e=>alert(e.message));
+      const promise = auth.createUserWithEmailAndPassword(email.value,password.value).then((usercredential)=>{
+        var user=usercredential.user;
+        promise.catch(e=>alert(e.message));
       alert("SignUp Successfully");
+      })
+      .catch((error)=>{
+        var errorCode=error.code;
+        var errorMessage=error.message;
+      });
     }
   
+  function writeData(){
+    var database = firebase.database();
+        var UserName=document.getElementById("UserName").value;
+        var Age=document.getElementById("Age").value;
+        function writeUserData(UserName, email, Age) {
+  firebase.database().ref('users').set({
+    username: UserName,
+    email: email,
+    Age: Age
+  });
+  }
+}
     //signIN function
     function  signIn(){
       var email = document.getElementById("email");
@@ -42,8 +60,8 @@ var firebaseConfig = {
     //active user to homepage
     firebase.auth().onAuthStateChanged((user)=>{
       if(user){
-        var email = user.email;
-        alert("Active user "+email);
+        var uid = user.uid;
+        alert("Active user ");
   
       }else{
         alert("No Active user Found")
